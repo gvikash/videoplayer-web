2c65101c864b2d86909cfd8038271513d9cbbf10
Vikash Gupta <vikashgupta@urbanclap.com>
2017-12-06 23:11:34 +0530

    Buttons added for fast forward and backward

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 var express = require('express');
 const directoryPath = './media';
 const fs = require('fs');
-var urlencode = require('urlencode');
-
 
 var app = express();
 
 var port = process.env.PORT || 5220;
 var filesArr = [];
 fs.readdirSync(directoryPath).forEach(function(file){
-  if( file.toLowerCase().indexOf('.mp4') > 0 || file.toLowerCase().indexOf('.avi') > 0 || file.toLowerCase().indexOf('.webp') > 0 || file.toLowerCase().indexOf('.mkv') > 0 ){
+  if( file.toLowerCase().indexOf('.mp4') > 0 || file.toLowerCase().indexOf('.avi') > 0 || file.toLowerCase().indexOf('.webp') > 0 || file.toLowerCase().indexOf('.webm') > 0 || file.toLowerCase().indexOf('.mkv') > 0 ){
     filesArr.push({path: '/' + file, name: file.replace(/\.[^/.]+$/, "")});
   }
 });
 
 app.use(express.static('public'));
-app.use(express.static('media'));
+app.use(express.static(directoryPath));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
